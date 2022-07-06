fa22de9d2da2ac69cfbb1c16277b80b2b16bb5b0
Jake Coffman <jake@jakecoffman.com>
2022-07-06 13:28:39 -0500

    yes sudo

--- justone.sh
+++ justone.sh
@@ -3,8 +3,8 @@
 go build cmd/justone/justonemain.go
 scp justonemain deploy@stldevs.com:~
 ssh deploy@stldevs.com << EOF
-  service justone stop
+  sudo service justone stop
   mv -f ~/justonemain /opt/justone/justone
   chmod +x /opt/justone/justone
-  servive justone start
+  sudo service justone start
 EOF
